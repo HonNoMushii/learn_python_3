f0145bb62b96ed49bf29cec91075ebc6c6a44780
HonNoMushii <stoutengijs@gmail.com>
2025-02-18 14:12:04 +0100

    Create exercise_13.py

---
+++ exercise_13/exercise_13.py
@@ -0,0 +1,13 @@
+#Parameters, unpacking, variables
+from sys import argv
+script, first, second, third = argv
+"""
+To run the script define and unpack the arguments/varriable
+
+python exercise_13.py arg1 arg2 arg3 
+"""
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
